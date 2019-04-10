04177be77d8c93b79dd39ca2932f3e756b2bc852
hmc-cs-ctan <ctan@cs.hmc.edu>
2019-04-10 15:03:14 -0700

    commit to pull

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import learning
 
 def main():
     client = learning.EpaClient('query_storage')
-    sql = 'SELECT * FROM `{}.air_quality_annual_summary` LIMIT 10;'
+    sql = 'SELECT y.latitude, y.longitude FROM (SELECT latitude, longitude, count(*) AS count_tuple FROM `{}.air_quality_annual_summary` GROUP BY latitude, longitude) y WHERE y.count_tuple = 8774;'
     df = client.query(sql)
+    df.to_csv("testcsv")
     print(df)
     print("Successful!")
 
