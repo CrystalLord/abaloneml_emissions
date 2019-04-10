25634ee3e5d6e52836bf6266b18d85c80ab43b96
hmc-cs-ctan <ctan@cs.hmc.edu>
2019-04-10 15:16:19 -0700

    fixed format error

--- main.py
+++ main.py
@@ -6,11 +6,37 @@ import learning
 
 def main():
     client = learning.EpaClient('query_storage')
-    sql = 'SELECT y.latitude, y.longitude FROM (SELECT latitude, longitude, count(*) AS count_tuple FROM `{}.air_quality_annual_summary` GROUP BY latitude, longitude) y WHERE y.count_tuple = 8774;'
-    df = client.query(sql)
-    df.to_csv("testcsv")
+    # sql = 'SELECT y.latitude, y.longitude FROM (SELECT latitude, longitude, count(*) AS count_tuple FROM `{}.air_quality_annual_summary` GROUP BY latitude, longitude) y WHERE y.count_tuple = 8774;'
+    # df = client.query(sql)
+    #sql = 'SELECT * FROM `{}.air_quality_annual_summary` LIMIT 10;'
+    #df = client.query(sql, dry_run=True)
+    df = query_hawkins(client)
     print(df)
     print("Successful!")
+
+
+def query_hawkins(client):
+    sql = '''SELECT
+                state_code,
+                county_code,
+                site_num,
+                date_local,
+                time_local,
+                parameter_name,
+                latitude, 
+                longitude,
+                sample_measurement,
+                mdl,
+                units_of_measure
+            FROM
+                `{}.o3_hourly_summary`
+            WHERE
+                (state_code = "06" AND
+                county_code = "065" AND
+                site_num = "8001");
+            '''
+    df = client.query(sql, dry_run=False)
+    return df
 
 
 def parseargs():
