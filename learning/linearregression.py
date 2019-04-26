ef04e4a278b6aed03bb456a68746edd29b2fba2a
AdamSchulze <adam.akira.schulze@gmail.com>
2019-04-26 13:19:06 -0700

    Added regularized regression models

--- learning/linearregression.py
+++ learning/linearregression.py
@@ -1,8 +1,18 @@
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 import numpy as np
 
 def regr(X,y):
     """Returns the coefficient of the linear regression."""
-    reg = LinearRegression()
+    reg = linear_model.LinearRegression()
+    reg.fit(X, y)
+    return reg
+
+def ridge(X,y, alpha=0.5):
+	reg = linear_model.Ridge(alpha=alpha)
+    reg.fit(X, y)
+    return reg
+
+def lasso(X,y, alpha=0.5):
+	reg = linear_model.Lasso(alpha=alpha)
     reg.fit(X, y)
     return reg
